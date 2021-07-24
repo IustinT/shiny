57ed98ee08b9040ef7186ddf8fe9e19608903be2
Iustin Trotus <iustin@trotus.co.uk>
2021-07-24 15:42:03 +0100

    custom logic for deserialing DateTime

--- src/Shiny.Core/Infrastructure/ShinySerializer.cs
+++ src/Shiny.Core/Infrastructure/ShinySerializer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Globalization;
 using Newtonsoft.Json;
+
 //using System.Text.Json;
 
 
@@ -8,7 +10,28 @@ namespace Shiny.Infrastructure
     public class ShinySerializer : ISerializer
     {
         public T Deserialize<T>(string value) => JsonConvert.DeserializeObject<T>(value);
-        public object Deserialize(Type objectType, string value) => JsonConvert.DeserializeObject(value, objectType);
+
+        public object Deserialize(Type objectType, string value)
+        {
+            try
+            {
+                if (objectType == typeof(DateTime) && DateTime
+                    .TryParseExact(value, "MM/dd/yyyy HH:mm:ss", null,
+                        DateTimeStyles.None, out var dtValue))
+                {
+                    return dtValue;
+                }
+
+
+                return JsonConvert.DeserializeObject(value, objectType);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
         public string Serialize(object value) => JsonConvert.SerializeObject(value);
         //public T Deserialize<T>(string value) => JsonSerializer.Deserialize<T>(value);
         //public object Deserialize(Type objectType, string value) => JsonSerializer.Serialize(value, objectType);
